66042fb51be9d7188e34299a4fd1ac9b42f3f832
Pavel Gritcatcuev <gritsatsuev.pavel@gmail.com>
2019-09-11 06:17:39 +0400

    drag'n'drop

--- src/app.js
+++ src/app.js
@@ -7,7 +7,69 @@ let locals = {
 }
 
 
-document.querySelector('main').innerHTML = (template(locals))
+document.querySelector('main').insertAdjacentHTML('afterbegin', template(locals));
+
+
+let dashboard = document.querySelector('.dashboard')
+
+
+
+dashboard.addEventListener('mousedown', function (event){
+  let elem = event.target.closest('li');
+  if (!elem ||event.target.closest('.task__done')) return
+  let y = event.pageY
+  let pseudo = document.createElement('div')
+  pseudo.className = 'pseudo'
+  let isPseudo = false
+
+  elem.style.width = elem.offsetWidth + 'px'
+  function firstMove () {
+    elem.before(pseudo)
+
+    isPseudo = true
+    elem.style.position = 'absolute';
+    elem.style.zIndex = 1000;
+    document.body.append(elem);
+  }
+  
+
+  moveAt(event.pageX, event.pageY);
+
+  
+  function moveAt(pageX, pageY) {
+    elem.style.left = pageX - elem.offsetWidth / 2 + 'px';
+    elem.style.top = pageY - elem.offsetHeight / 2 + 'px';
+  }
+
+  function onMouseMove(event) {
+    if (!isPseudo) firstMove()
+    moveAt(event.pageX, event.pageY);
+    elem.hidden = true;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    elem.hidden = false;
+    
+    let droppableBelow = elemBelow.closest('li');
+    
+    if(event.pageY < y&&droppableBelow) {
+      droppableBelow.before(pseudo)
+    }else if(event.pageY > y&&droppableBelow) {
+      droppableBelow.after(pseudo)
+    }
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  
+  
+  elem.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    elem.onmouseup = null;
+    elem.style.position = 'static'
+    elem.style.width = 100+'%'
+    pseudo.after(elem)
+    pseudo.remove()
+  };
+})
+
 
 
 
